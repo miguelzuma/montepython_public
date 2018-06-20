81788c56e89749f7bff0831c5b2dffe5c8d7da75
ardok-m <carlosgarcia@iff.csic.es>
2018-06-20 10:42:10 +0200

    fixed error storing derived parameters

--- montepython/cosmo_hammer.py
+++ montepython/cosmo_hammer.py
@@ -235,7 +235,7 @@ class DerivedUtil(SampleFileUtil):
 
 def store_cosmo_derived(ctx):
     """
-    Store derived parameters. Copied from sampler.compute_lkl
+    Store derived parameters. Based on sampler.compute_lkl
     """
     from classy import CosmoSevereError
 
@@ -246,8 +246,6 @@ def store_cosmo_derived(ctx):
         try:
             derived = cosmo.get_current_derived_parameters(
                 data.get_mcmc_parameters(['derived']))
-            for name, value in derived.iteritems():
-                data.mcmc_parameters[name]['current'] = value
         except AttributeError:
             # This happens if the classy wrapper is still using the old
             # convention, expecting data as the input parameter
@@ -255,6 +253,9 @@ def store_cosmo_derived(ctx):
         except CosmoSevereError:
             raise io_mp.CosmologicalModuleError(
                 "Could not write the current derived parameters")
-    for elem in data.get_mcmc_parameters(['derived']):
-        data.mcmc_parameters[elem]['current'] /= \
-            data.mcmc_parameters[elem]['scale']
+
+    # If we want the derived parameters stored rescaled
+    # derived_scaled = {elem: value/data.mcmc_parameters[elem]['scale'] for elem, value in derived.iteritems()}
+    # ctx.add('key_data', derived_scaled)
+
+    ctx.add('key_data', derived)
